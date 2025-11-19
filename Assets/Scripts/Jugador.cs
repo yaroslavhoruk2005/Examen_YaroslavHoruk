387e335e1ae2ebe66dba55b114431375cb44bf0c
Yaroslav Horuk <yhor0807@g.educaand.es>
2025-11-19 18:34:51 +0100

    Cuarto Commit

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
+
 public class Jugador : MonoBehaviour
 {
     [Header("----------Movimiento----------")]
@@ -11,25 +13,72 @@ public class Jugador : MonoBehaviour
     public float fuerzaSalto = 7f;
     private bool enSuelo = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("----------Chequeo de suelo----------")]
+    public Transform comprobadorSuelo; 
+    public float radioSuelo = 0.2f;
+    public LayerMask capaSuelo;
+
+    [Header("----------Sonido   ----------")]
+    public AudioSource audioSource;       // arrastra aquí el AudioSource
+    public AudioClip getMonedas;
+
+    private bool mirandoDerecha = true;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
+
     private void OnMove(InputValue inputMovimiento)
     {
         movimientoX = inputMovimiento.Get<Vector2>().x;
+    }
 
-        if (movimientoX != 0)
+    private void OnJump(InputValue inputSalto)
+    {
+        if (enSuelo)
         {
-            transform.localScale = new Vector3(Mathf.Sign(movimientoX), 1, 1);
+            rb2d.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
         }
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        
+        enSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, radioSuelo, capaSuelo);
+
+        if (movimientoX > 0 && !mirandoDerecha)
+        {
+            Girar();
+        }
+        else if (movimientoX < 0 && mirandoDerecha)
+        {
+            Girar();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.linearVelocity = new Vector2(movimientoX * velocidad, rb2d.linearVelocity.y);
+    }
+
+    void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Moneda"))
+        {
+            if (audioSource != null && getMonedas != null)
+            {
+                audioSource.PlayOneShot(getMonedas);
+            }
+
+            GameManager.Instance.SumarMonedas();
+            Destroy(collision.gameObject);
+        }
     }
 }
